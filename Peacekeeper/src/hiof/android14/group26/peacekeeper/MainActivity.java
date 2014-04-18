57778daa73d192000a2ddfb2d12c9631aaf6f269
Per Edvard Volla <perev@hiof.no>
2014-04-18 16:02:52 +0200

    Hopefully it works...

--- Peacekeeper/src/hiof/android14/group26/peacekeeper/MainActivity.java
+++ Peacekeeper/src/hiof/android14/group26/peacekeeper/MainActivity.java
@@ -16,6 +16,8 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
